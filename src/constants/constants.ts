ef81a41f6fa94b2e31bbcdd9ab908933f47d6451
pradattaaryal <pradattaaryal2468@gmail.com>
2025-01-18 17:38:07 +0545

    clean architecture react

---
+++ src/constants/constants.ts
@@ -0,0 +1,1 @@
+export const CURRENT_BASE_URL = "https://localhost:7133/api/Employee";
--- src/infrastructure/http/HttpClient.ts
+++ src/infrastructure/http/HttpClient.ts
@@ -1,6 +1,73 @@
+import { CURRENT_BASE_URL } from "../../constants/constants";
 import axios, { AxiosInstance, AxiosResponse } from "axios";
-// import { CURRENT_BASE_URL } from "../../constants/constants";
 
+
+
+export abstract class HttpClient {
+  protected instance: AxiosInstance | undefined;
+
+  protected createInstance(): AxiosInstance {
+    this.instance = axios.create({
+      baseURL: CURRENT_BASE_URL,
+    });
+
+    this.initializeInterceptors();
+    return this.instance;
+  }
+
+  private initializeInterceptors = () => {
+    // Intercept response to handle success and error
+    this.instance?.interceptors.response.use(
+      this.handleResponse,
+      this.handleError
+    );
+
+    // Intercept request to add token and dynamically set Content-Type
+    this.instance?.interceptors.request.use((config: any) => {
+      const token = localStorage.getItem("jwtToken");
+
+      // Add Authorization header if token exists
+      if (token) {
+        config.headers = {
+          ...config.headers,
+          Authorization: `Bearer ${token}`,
+        };
+      }
+
+      // Dynamically set Content-Type header
+      if (config.data instanceof FormData) {
+        config.headers["Content-Type"] = "multipart/form-data";
+      } else {
+        config.headers["Content-Type"] = "application/json";
+      }
+
+      return config;
+    });
+  };
+
+  private handleResponse = (response: AxiosResponse) => {
+    return response;
+  };
+
+  private handleError = (error: any) => {
+    console.error("API Error:", error);
+
+    if (error.response) {
+      return Promise.reject({
+        status: error.response.status,
+        message: error.response.data?.message || "Something went wrong",
+        errors: error.response.data?.errors || null,
+      });
+    }
+
+    return Promise.reject({
+      status: 500,
+      message: "Network error, please try again later",
+    });
+  };
+}
+
+/*import axios, { AxiosInstance, AxiosResponse } from "axios";
 const CURRENT_BASE_URL = "https://localhost:7133/api/Employee";
 
 export abstract class HttpClient {
@@ -55,4 +122,7 @@ export abstract class HttpClient {
       message: "Network error, please try again later",
     });
   };
+
+ 
 }
+*/
